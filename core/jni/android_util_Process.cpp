1faa701e9959ba2bf9e0095689598d3b9d37b3e0
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-05 20:06:27 +0800

    Boost framework: CPU props integration
    Change-Id: I94688cfd7defdc809f2f64e97d7ee696527e65a9
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- core/jni/android_util_Process.cpp
+++ core/jni/android_util_Process.cpp
@@ -29,6 +29,7 @@
 #include <processgroup/processgroup.h>
 #include <processgroup/sched_policy.h>
 #include <android-base/logging.h>
+#include <android-base/properties.h>
 #include <android-base/unique_fd.h>
 
 #include <algorithm>
@@ -231,6 +232,56 @@ void android_os_Process_setThreadGroupAndCpuset(JNIEnv* env, jobject clazz, int 
 
     if (res != NO_ERROR) {
         signalExceptionForGroupError(env, -res, tid);
+    }
+}
+
+void android_os_Process_setThreadAffinity(JNIEnv* env, jobject clazz, int tid, jint grp) {
+    cpu_set_t target_cpu_set;
+    CPU_ZERO(&target_cpu_set);
+
+    std::vector<int32_t> small_cores, big_cores;
+
+    auto parseCpusets = [](const std::string& cpuset_str, std::vector<int32_t>& cpus) {
+        std::istringstream ss(cpuset_str);
+        std::string token;
+        while (std::getline(ss, token, ',')) {
+            char* endptr;
+            long cpu = std::strtol(token.c_str(), &endptr, 10);
+            if (*endptr == '\0' && cpu >= 0) {
+                cpus.push_back(static_cast<int32_t>(cpu));
+            } else {
+                ALOGI("Invalid CPU core value: %s", token.c_str());
+            }
+        }
+    };
+
+    parseCpusets(android::base::GetProperty("persist.sys.axion_cpu_small", "0,1,2,3"), small_cores);
+    parseCpusets(android::base::GetProperty("persist.sys.axion_cpu_big", "4,5,6,7"), big_cores);
+
+    if (grp == 1) {
+        for (int core : small_cores) {
+            CPU_SET(core, &target_cpu_set);
+        }
+    } 
+    else if (grp == 0) {
+        for (int core : big_cores) {
+            CPU_SET(core, &target_cpu_set);
+        }
+    } 
+    else if (grp == 2) {
+        int max_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+        for (int i = 0; i < max_cpus; i++) {
+            CPU_SET(i, &target_cpu_set);
+        }
+    } 
+    else {
+        return;
+    }
+
+    if (sched_setaffinity(tid, sizeof(cpu_set_t), &target_cpu_set) == -1) {
+        ALOGI("Failed to set CPU affinity for thread %d: %s", tid, strerror(errno));
+    } else {
+        ALOGI("Successfully set affinity for thread %d", tid);
     }
 }
 
