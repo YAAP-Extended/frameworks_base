2d62faabe0d4b07d1d065ca8f218ca53d490747a
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-24 10:50:21 +0800

    ScreenshotController: Return package name if label is not available
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: FATAL EXCEPTION: main
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: Process: com.android.systemui:screenshot, PID: 13586
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: android.content.pm.PackageManager$NameNotFoundException: ComponentInfo{com.android.systemui/class com.android.systemui.SystemUIService}
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: 	at android.app.ApplicationPackageManager.getActivityInfo(ApplicationPackageManager.java:604)
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: 	at android.app.ApplicationPackageManager.getActivityInfo(ApplicationPackageManager.java:587)
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: 	at com.android.systemui.screenshot.ScreenshotController.handleScreenshot(go/retraceme 01afb4a44aa996f6a8549bb9ae0631f1038f019e1bc51f730b3545bef24392b2:17)
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: 	at com.android.systemui.screenshot.TakeScreenshotExecutorImpl.dispatchToController(go/retraceme 01afb4a44aa996f6a8549bb9ae0631f1038f019e1bc51f730b3545bef24392b2:188)
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: 	at com.android.systemui.screenshot.TakeScreenshotExecutorImpl$dispatchToController$1.invokeSuspend(go/retraceme 01afb4a44aa996f6a8549bb9ae0631f1038f019e1bc51f730b3545bef24392b2:17)
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: 	at kotlin.coroutines.jvm.internal.BaseContinuationImpl.resumeWith(go/retraceme 01afb4a44aa996f6a8549bb9ae0631f1038f019e1bc51f730b3545bef24392b2:8)
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: 	at kotlinx.coroutines.DispatchedTask.run(go/retraceme 01afb4a44aa996f6a8549bb9ae0631f1038f019e1bc51f730b3545bef24392b2:110)
    03-24 10:44:19.060 13586 13586 E AndroidRuntime: 	at android.os.Handler.handleCallback(Handler.java:991)
    
    Change-Id: Ia5589c0e70186481f546803ea5c16964f04e40e0

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.kt
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.kt
@@ -162,6 +162,18 @@ internal constructor(
         Assert.isMainThread()
         screenshotHandler.resetTimeout()
 
+        packageLabel = runCatching {
+            val info = packageManager.getApplicationInfo(screenshot.packageNameString, 0)
+            info.loadLabel(packageManager).toString()
+        }.getOrDefault(screenshot.packageNameString)
+        scrollCaptureExecutor.longScreenshotHolder.foregroundAppName = packageLabel
+
+        if (screenshot.type == WindowManager.TAKE_SCREENSHOT_SELECTED_REGION) {
+            startPartialScreenshotActivity(Process.myUserHandle())
+            finisher.accept(null)
+            return
+        }
+
         val currentBitmap = screenshot.bitmap
         if (currentBitmap == null) {
             Log.e(TAG, "handleScreenshot: Screenshot bitmap was null")
